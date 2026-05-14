ce90052678a450e8b25df8b89bd5a78fa774c49f
daniilderenkov7-sys <daniilderenkov7@gmail.com>
2026-05-14 18:38:52 +0300

    Add files via upload

---
+++ lab2-structures-dan/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+// 3. Оптимизированное умножение
+void optimizedMultiply(const vector<double>& A,
+                       const vector<double>& B,
+                       vector<double>& C)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            double temp = A[i * N + k];
+
+            for (int j = 0; j < N; j++)
+            {
+                C[i * N + j] += temp * B[k * N + j];
+            }
+        }
+    }
+}
+
+void benchmark(void (*func)(const vector<double>&,
+                            const vector<double>&,
+                            vector<double>&),
+               const vector<double>& A,
+               const vector<double>& B,
+               vector<double>& C,
+               const string& name)
+{
+    auto start = high_resolution_clock::now();
+
+    func(A, B, C);
+
+    auto end = high_resolution_clock::now();
+
+    double time = duration<double>(end - start).count();
+
+    double complexity = 2.0 * N * N * N;
+    double mflops = complexity / (time * 1e6);
+
+    cout << fixed << setprecision(3);
+    cout << "\n" << name << endl;
+    cout << "Time: " << time << " sec" << endl;
+    cout << "MFLOPS: " << mflops << endl;
+}
+
+int main()
+{
+    vector<double> A(N * N);
+    vector<double> B(N * N);
+    vector<double> C(N * N, 0.0);
+
+    generateMatrix(A);
+    generateMatrix(B);
+
+    benchmark(classicMultiply, A, B, C, "Classic Multiply");
+
+    fill(C.begin(), C.end(), 0.0);
+    benchmark(blasMultiply, A, B, C, "BLAS Multiply");
+
+    fill(C.begin(), C.end(), 0.0);
+    benchmark(optimizedMultiply, A, B, C, "Optimized Multiply");
+
+    return 0;
+}
